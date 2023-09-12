c68229fb97846210d2cff8617b1d8c37d27029bd
rafawaltrick <rafawaltrick@gmail.com>
2023-09-12 08:34:44 -0300

    Criado estrutura de pastas.

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function Routes(){
+    const Stack = createNativeStackNavigator();
+    return(
+        <NavigationContainer>
+            
+        </NavigationContainer>
+    )
+}
