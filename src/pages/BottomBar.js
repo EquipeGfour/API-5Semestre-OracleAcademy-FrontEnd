e4d85a4a5fd69ec6c364311ad086b8ca4a2af662
lirabessa <natalirabessa@gmail.com>
2023-09-17 11:02:42 -0300

    Finalizado modal do bottom bar

--- src/pages/BottomBar.js
+++ src/pages/BottomBar.js
@@ -3,12 +3,14 @@ import { View, TouchableOpacity, Text, StyleSheet, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import Modal from 'react-native-modal';
 import { useNavigation } from '@react-navigation/native';
+import { TextInput } from 'react-native-paper';
 
 const verdeEscuro = "#346c68";
 
 const BottomBar = ({ onIconPress }) => {
     const navigation = useNavigation();
     const [isModalVisible, setModalVisible] = useState(false);
+    const [text, setText] = React.useState('');
 
     const openModal = () => {
         setModalVisible(true);
@@ -32,8 +34,9 @@ const BottomBar = ({ onIconPress }) => {
 
             <Modal isVisible={isModalVisible}>
                 <View style={styles.modalContainer}>
-                    <Text>Conteúdo do Modal</Text>
-                    <Button title="Fechar Modal" onPress={closeModal} />
+                <TextInput style = {styles.modalText} multiline={true} placeholder='Nome' label = "Nome" />
+                <TextInput style = {styles.modalText} multiline={true} placeholder='Descrição' label = "Descrição"/>
+                <Button title="Fechar" onPress={closeModal} color = {verdeEscuro}/>
                 </View>
             </Modal>
         </View>
@@ -61,7 +64,15 @@ const styles = StyleSheet.create({
         padding: 20,
         borderRadius: 10,
         alignItems: 'center',
+        
     },
+    modalText: {
+        mode:"flat",
+        backgroundColor : "white",
+        width: 200,
+        marginBottom: 30
+    },
+    
 });
 
 export default BottomBar;
