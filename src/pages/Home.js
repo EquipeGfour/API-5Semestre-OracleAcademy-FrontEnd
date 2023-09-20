1e5652e1f5f91380fa0a8253a587f57ee0965062
lirabessa <natalirabessa@gmail.com>
2023-09-20 11:10:57 -0300

    Implementado home na tabMenu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,7 @@ import { StyleSheet, View, ScrollView } from 'react-native';
 import { Text } from "react-native-paper";
 import BottomBar from "./BottomBar";
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {ProgressBar , Colors} from 'react-native-paper'
-
+import { ProgressBar, Colors, Card,IconButton, Avatar } from 'react-native-paper'
 
 const verdeEscuro = "#346c68";
 
@@ -13,23 +12,25 @@ const Home = ({ }) => {
         <>
             <BottomBar style={{ flex: 1 }} />
             <ScrollView horizontal={true} contentContainerStyle={styles.container}>
-                <View style={[styles.retangulo, styles.rosa]}>
-                    <Text>Relátorios</Text>
-                    <View style = {styles.informacoes}>
-                        <Icon name="clock" size={30} color={verdeEscuro}/>
-                        <View style={{ width: 10 }} /> 
-                        <Text>20/10/2013</Text>
+                <View style={[styles.retangulo, styles.verdeEscuro]}>
+                    <Card.Title 
+                        title ="Relatorio"
+                    />
+                    <Card.Title 
+                        title ="11/10/2023"
+                        left={(props) => <Avatar.Icon {... props} icon = "clock"/>}
+                        />
+                    <Card.Title 
+                        title ="Alta"
+                        left={(props) => <Avatar.Icon {... props} icon = "flag"/>}
+                        />
+
+                        <Card.Content>
+                            <Text style = {styles.textoNome}>Progresso</Text>
+                            <Text style = {styles.textoPorcentagem}>40%</Text>
+                            <ProgressBar progress={0.5} color='#9BA5B7' style={{ backgroundColor: 'white' }} />
+                        </Card.Content>
                     </View>
-                    <View style = {styles.informacoes2}>
-                        <Icon name="flag" size={30} color={verdeEscuro} />
-                        <Text>Alta</Text>
-                        <View style={{ width: 60 }}  />
-                    </View>
-                        <ProgressBar style= {styles.progressBar} progress={0.5} color = {verdeEscuro} style= {{backgroundColor: 'red'}}/>
-                </View>
-                <View style={[styles.retangulo, styles.verde]}></View>
-                <View style={[styles.retangulo, styles.rosa]}></View>
-                <View style={[styles.retangulo, styles.verde]}></View>
             </ScrollView>
         </>
     );
@@ -37,35 +38,49 @@ const Home = ({ }) => {
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row', 
+        flexDirection: 'row',
     },
     retangulo: {
-        width: 153,
-        height: 200,
+        width: 200,
+        height: 250,
         margin: 15,
-        borderRadius: 20
-    },
-    rosa: {
-        backgroundColor: 'pink', 
-    },
-    verde: {
-        backgroundColor: 'green',
-    },
-    informacoes:{
-        justifyContent:"center",
-        paddingTop: '30%',
-        flexDirection: 'row', alignItems: "center"
+        borderRadius: 20,
         
     },
-    informacoes2:{
-        justifyContent:"center",
-        paddingTop: '5%',
-        flexDirection: 'row', alignItems: "center"
-        
+    verdeEscuro: {
+        backgroundColor: "#51A8A2",
     },
-    progressBar:{
-        
+    informacoes: {
+        alignSelf: "stretch",
+        paddingTop: '10%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    informacoes2: {
+        alignSelf: "stretch",
+        paddingTop: '10%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    progressBar: {
+        alignSelf: "stretch",
+        width: '80%',
 
+    },
+    tituloRetangulo: {
+        fontSize: 20,
+        marginTop: '10%',
+        paddingRight: 40,
+        color: 'white',
+    },
+    textoIcone: {
+        color: 'white'
+    },
+    textoPorcentagem:{
+        alignSelf:"flex-end"
+    },
+    textoNome:{
+        alignSelf: "flex-start"
     }
 });
 
