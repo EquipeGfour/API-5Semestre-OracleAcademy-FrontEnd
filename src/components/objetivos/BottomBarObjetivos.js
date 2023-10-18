05f27aeac102b0440ea9751f4b439bb3ab581918
lirabessa <natalirabessa@gmail.com>
2023-10-18 09:45:34 -0300

    Adicionado DataPicker

--- src/components/objetivos/BottomBarObjetivos.js
+++ src/components/objetivos/BottomBarObjetivos.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Button, DataPikerIOS } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import Modal from 'react-native-modal';
 import { useNavigation } from '@react-navigation/native';
-import { TextInput, PaperProvider, Provider,DefaultTheme  } from 'react-native-paper';
+import { TextInput, PaperProvider, DefaultTheme } from 'react-native-paper';
 import DropdownComponent from './DropDownPrioridadeObjetivo';
-import BemVindo from '../../pages/BemVindo';
 import { postObjetivos } from '../../service/objetivo';
 import { getStorageItem } from '../../functions/encryptedStorageFunctions';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import DataPicker from '../dataPicker';
 
 const colors = {
     verde: "#51A8A2",
@@ -22,7 +21,7 @@ const theme = {
     ...DefaultTheme,
     colors: {
         ...DefaultTheme.colors,
-      primary: colors.verde, // Cor de foco
+        primary: colors.verde, // Cor de foco
     },
 };
 
@@ -33,21 +32,23 @@ const BottomBarObjetivos = ({ onIconPress }) => {
 
     const [nome, setNome] = useState("");
     const [descricao, setDescricao] = useState("");
-    const [dataEstimada, setDataEstimada] = useState("");
+    const [dataEstimada, setDataEstimada] = useState(new Date()); // Inicialize com a data atual
     const [prioridade, setPrioridade] = useState("");
 
-    const criarObjetivo = async () =>{
-        const obj = {titulo: nome, descricao:descricao, data_estimada: dataEstimada, prioridade:prioridade, workspace:false}
-        const token = await getStorageItem('token');       
-        postObjetivos(obj,token).then((res) => {
-            setNome('')
-            setDescricao('')
-            setDataEstimada('')
-            setPrioridade('')
-            closeModal(false)
-        }).catch(error => {
-            console.error('Erro', error);
-        });
+    const criarObjetivo = async () => {
+        const obj = { titulo: nome, descricao: descricao, data_estimada: dataEstimada, prioridade: prioridade, workspace: false };
+        const token = await getStorageItem('token');
+        postObjetivos(obj, token)
+            .then((res) => {
+                setNome('')
+                setDescricao('')
+                setDataEstimada(new Date()) // Reinicie a data para a atual
+                setPrioridade('')
+                closeModal(false)
+            })
+            .catch(error => {
+                console.error('Erro', error);
+            });
     }
 
     const openModal = () => {
@@ -58,54 +59,46 @@ const BottomBarObjetivos = ({ onIconPress }) => {
         setModalVisible(false);
     };
 
-
     return (
         <View style={styles.container}>
-            {/* <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.icon}>
-                <Icon name="home" size={30} color={colors.verde} />
-            </TouchableOpacity> */}
             <TouchableOpacity onPress={openModal} style={styles.icon}>
                 <Icon name="plus-circle" size={30} color={colors.verde} />
             </TouchableOpacity>
-            {/* <TouchableOpacity onPress={() => onIconPress('Login')} style={styles.icon}>
-                <Icon name="chart-bar" size={30} color={colors.verde} />
-            </TouchableOpacity> */}
 
             <Modal isVisible={isModalVisible} onBackdropPress={closeModal}>
-            <PaperProvider theme={theme}>
-                <View style={styles.modalContainer}>
-                    {/* <Text style = {{fontSize: 20}}>Criar Objetivos</Text> */}
-                    <TextInput style = {styles.modalText}
-                        mode='outlined'
-                        label= 'Nome'
-                        multiline={true} 
-                        placeholder='Nome'
-                        value={nome} onChangeText={(e) => setNome(e)}/>
-                    {/* <TextInput style = {styles.modalText}  
-                        mode='outlined'
-                        label= 'Descrição'
-                        multiline={true} 
-                        placeholder='Descrição' 
-                        value={descricao} onChangeText={(e) => setDescricao(e)}/> */}
-                    <TextInput style = {styles.modalText}
-                        mode='outlined'
-                        label= 'Data de Conclusão'
-                        multiline={true} 
-                        placeholder='DD/MM/AAAA'
-                        value={dataEstimada} onChangeText={(e) => setDataEstimada(e)}
-                    />
-                    <DropdownComponent prioridade={prioridade} setPrioridade={setPrioridade} style = {styles.modalText}/>
-                
-                
-                    <View style={{marginTop: 20}}>         
-                        <TouchableOpacity onPress={criarObjetivo} style={styles.botaoCriar}>
-                            <Text style={styles.buttonText}>Criar</Text>
-                        </TouchableOpacity>
-                        {/* <Button title="Adicionar Objetivo" onPress={criarObjetivo} color = {colors.verde}/> */}
+                <PaperProvider theme={theme}>
+                    <View style={styles.modalContainer}>
+                        <TextInput
+                            style={styles.modalText}
+                            mode='outlined'
+                            label='Nome'
+                            placeholder='Nome'
+                            value={nome}
+                            onChangeText={(e) => setNome(e)}
+                        />
+                        <TextInput
+                            style={styles.modalText}
+                            mode='outlined'
+                            label='Descrição'
+                            multiline={true}
+                            placeholder='Descrição'
+                            value={descricao}
+                            onChangeText={(e) => setDescricao(e)}
+                        />
+                        <DataPicker
+                            selectedDate={dataEstimada}
+                            onSelectDate={(date) => setDataEstimada(date)}
+                        />
+                        <DropdownComponent prioridade={prioridade} setPrioridade={setPrioridade} style={styles.modalText} />
+
+                        <View style={{ marginTop: 20 }}>
+                            <TouchableOpacity onPress={criarObjetivo} style={styles.botaoCriar}>
+                                <Text style={styles.buttonText}>Criar</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
-            </PaperProvider>
-            </Modal> 
+                </PaperProvider>
+            </Modal>
         </View>
     );
 };
@@ -161,5 +154,4 @@ const styles = StyleSheet.create({
     
     
 });
-
 export default BottomBarObjetivos;
